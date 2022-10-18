37b6b58b59c52eea6b01dd2391e1c329329444f4
Suleman Hajizada <suleman0708@gmail.com>
2022-10-18 11:22:02 +0200

    new repo

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Apple Redesign</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Lets do yhis</h1>
+    </div>
+  )
+}
+
+export default Home
