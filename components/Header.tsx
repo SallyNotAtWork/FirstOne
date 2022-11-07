44e357552fdabd9eed0676804ad331dbf476d2cb
Suleman Hajizada <suleman@elkjop.no>
2022-11-07 11:11:09 +0100

    fixed position header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,38 +4,18 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="flex">
+    <div className="flex flex-row items-center px-4 py-6 sm:px-6 space-x-5">
 
-
-      {/* <div className="flex items-center justify-center md:w-1/8">
-        <Link href="/">
-          <div className="relative w-10 cursor-pointer opacity-75 h-10 transition hover:opacity-100">
-            <Image
-              src={"/dogpaw.png"}
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-        </Link>
-      </div>
-
-      <div className="p-4">
-        HUND I OSLO - ENKLERE LIV MED HUND
-      </div> */}
-
-      
-      <div className="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
-
-      
+      <div className="flex-none ">
        <a href="/">
-            <span className="sr-only">Your Company</span>
             <img
-              className="h-8 w-auto sm:h-10"
+              className="h-8 w-auto"
               src="/dogpaw.png"
               alt=""
             />
         </a>
-       
+        </div>
+        
         <a className="headerLink" href="/">
           HUND I OSLO - HVOR KAN DU GÅ?
         </a>
@@ -50,7 +30,7 @@ function Header() {
 
 
       </div>
-    </header>
+ 
   );
 }
 
